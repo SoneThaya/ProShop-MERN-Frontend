cf4284ec001f23bcd39dc0059121bea49be8367a
Sone <sthaya@gmail.com>
2020-11-07 20:22:20 -0800

    styled login and setup functionality

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,13 +7,56 @@ import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
 import { login } from "../actions/userAction";
 
-const LoginScreen = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
+const LoginScreen = ({ location }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return <div>
-    
-    </div>;
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // Dispatch Login
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
