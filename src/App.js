f7d1a4dc3aa5a620f2143745f4dd4637d735dee9
Sone <sthaya@gmail.com>
2020-11-15 21:04:16 -0800

    create paginate component to handle the backend

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
           <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
-          <Route path="/search/:keyword" component={HomeScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} exact />
           <Route path="/page/:pageNumber" component={HomeScreen} exact />
           <Route
             path="/search/:keyword/page/:pageNumber"
